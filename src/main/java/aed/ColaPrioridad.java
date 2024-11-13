ca75361f896fb6b356053d14b6406052ca157d86
Nehuen Ivanovic <neheunigds@outlook.com>
2024-11-13 00:03:04 -0300

    cola de prioridad no implementada

---
+++ src/main/java/aed/ColaPrioridad.java
@@ -0,0 +1,28 @@
+package aed;
+
+public class ColaPrioridad {
+
+    public  ColaPrioridad(){
+    }
+    public ColaPrioridad colaPrioridadDesdeSecuencia(){
+        return null;
+    }
+    public void encolar(){
+        //Implementar
+    }
+    public Nodo maximo(){
+        //Implementar
+        return null;
+    }
+    public void desencolarMaximo(){
+        //implementar
+    }
+    public void cambiarPrioridad(){
+        //nose si es necesario implementar
+    }
+    public int tamano(){
+        //nose si es necesario implementar
+        return 0;
+    }
+
+}
