4a86d76e7109ba6bdf79d36a98ff3b0c5fb675a4
Nehuen Ivanovic <neheunigds@outlook.com>
2024-11-17 23:18:56 -0300

    algunas complejidads

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -11,51 +11,51 @@ public class BestEffort {
 
     public BestEffort(int cantCiudades, Traslado[] traslados) {
         // crear DobleCola
-        this.traslados = new ColaDoble(traslados);
+        this.traslados = new ColaDoble(traslados); //O(log|traslados|)
         // crear Stats
-        this.stats = new Stats(cantCiudades);
+        this.stats = new Stats(cantCiudades); //O(log|cantCiudades|)
     }
 
     public void registrarTraslados(Traslado[] traslados) {
-        for (int i = 0; i < traslados.length; i++) {
-            this.traslados.encolar(traslados[i]);
+        for (int i = 0; i < traslados.length; i++) { //O (|traslados|log|T|)
+            this.traslados.encolar(traslados[i]); //O(log|T|)
         }
     }
 
     public int[] despacharMasRedituables(int n) {
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            Traslado t = this.traslados.desencolarRedito();
-            stats.add(t);
-            res[i] = t.id;
+        int[] res = new int[n]; //O(n)
+        for (int i = 0; i < n; i++) { //log(n(log|T| + log|C|))
+            Traslado t = this.traslados.desencolarRedito(); //O(log|T|)
+            stats.add(t); //O(log|C|)
+            res[i] = t.id; //O(1)
         }
         return res;
     }
 
     public int[] despacharMasAntiguos(int n) {
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            Traslado t = this.traslados.desencolarAntiguedad();
-            stats.add(t);
-            res[i] = t.id;
+        int[] res = new int[n];//O(n)
+        for (int i = 0; i < n; i++) {//log(n(log|T| + log|C|))
+            Traslado t = this.traslados.desencolarAntiguedad();//O(log|T|)
+            stats.add(t); //O(log|C|)
+            res[i] = t.id;//O(1)
         }
         return res;
     }
 
     public int ciudadConMayorSuperavit() {
-        return stats.getMayorSuperavit();
+        return stats.getMayorSuperavit();//O(1)
     }
 
     public ArrayList<Integer> ciudadesConMayorGanancia() {
-        return stats.getCiudadesConMayorGanancias();
+        return stats.getCiudadesConMayorGanancias();//O(1)
     }
 
     public ArrayList<Integer> ciudadesConMayorPerdida() {
-        return stats.getCiudadesConMayorPerdida();
+        return stats.getCiudadesConMayorPerdida();//O(1)
     }
 
     public int gananciaPromedioPorTraslado() {
-        return stats.getPromedio();
+        return stats.getPromedio();//O(1)
     }
 
 }
--- src/main/java/aed/Stats.java
+++ src/main/java/aed/Stats.java
@@ -16,14 +16,14 @@ public class Stats {
         mayoresGanancia = new ArrayList<>();
         mayoresPerdida = new ArrayList<>();
         ciudades = new ArrayList<>();
-        for (int i = 0; i < cantCiudades; i++) {
-            ciudades.add(new Ciudad(i));
+        for (int i = 0; i < cantCiudades; i++) {//O(|cantCiudades|)
+            ciudades.add(new Ciudad(i));//O(1)
         }
-        superavits = new ColaPrioridadCiudades(ciudades);
+        superavits = new ColaPrioridadCiudades(ciudades);//O(|cantCiudades|)
     }
 
 
-    public void add(Traslado t) {
+    public void add(Traslado t) {//O(log|C|)
         //actualizar perdidas y ganancias
         Integer idO = t.origen;
         int g = this.ciudades.get(idO).getGanancia();
@@ -32,56 +32,56 @@ public class Stats {
         int p = this.ciudades.get(idD).getPerdida();
         p+= t.gananciaNeta;        
         
-        if(mayoresGanancia.isEmpty()){
+        if(mayoresGanancia.isEmpty()){//O(1)
             mayoresGanancia.add(idO);
         }
-        else if (ciudades.get(mayoresGanancia.get(0)).getGanancia() < g) {
-            mayoresGanancia.clear();
-            mayoresGanancia.add(idO);
-        } else if (ciudades.get(mayoresGanancia.get(0)).getGanancia()  == g) {
-            mayoresGanancia.add(idO);
+        else if (ciudades.get(mayoresGanancia.get(0)).getGanancia() < g) {//O(1)
+            mayoresGanancia.clear();//O(1)
+            mayoresGanancia.add(idO);//O(1)
+        } else if (ciudades.get(mayoresGanancia.get(0)).getGanancia()  == g) {//O(1)
+            mayoresGanancia.add(idO);//O(1)
         } // si es menor no hago nada
         
-        if(mayoresPerdida.isEmpty()){
-            mayoresPerdida.add(idD);
+        if(mayoresPerdida.isEmpty()){//O(1)
+            mayoresPerdida.add(idD);//O(1)
         }
-        else if (ciudades.get(mayoresPerdida.get(0)).getPerdida() < p) {
-            mayoresPerdida.clear();
-            mayoresPerdida.add(idD);
+        else if (ciudades.get(mayoresPerdida.get(0)).getPerdida() < p) {//O(1)
+            mayoresPerdida.clear();//O(1)
+            mayoresPerdida.add(idD);//O(1)
         } else if (ciudades.get(mayoresPerdida.get(0)).getPerdida() == p) {
-            mayoresPerdida.add(idD);
+            mayoresPerdida.add(idD);//O(1)
         } // si es menor no hago nada
         
-        ciudades.get(idO).addToGanancia(t.gananciaNeta);
-        ciudades.get(idD).addToPerdida(t.gananciaNeta);
+        ciudades.get(idO).addToGanancia(t.gananciaNeta);//O(1)
+        ciudades.get(idD).addToPerdida(t.gananciaNeta);//O(1)
         
         //actualizar superavits
         //sumar en origen
-        Ciudad ciudadO = ciudades.get(t.origen);
+        Ciudad ciudadO = ciudades.get(t.origen);//O(1)
         //eliminarlos de superavits
-        superavits.borrar_ciudad(ciudadO);
-        superavits.encolar(ciudadO);
+        superavits.borrar_ciudad(ciudadO);//O(log|C|)
+        superavits.encolar(ciudadO);//O(log|C|)
         Ciudad ciduadD = ciudades.get(t.destino);
-        superavits.borrar_ciudad(ciduadD);
-        superavits.encolar(ciduadD);
+        superavits.borrar_ciudad(ciduadD);//O(log|C|)
+        superavits.encolar(ciduadD);//O(log|C|)
         //actualizar suma y cantidad despacho
-        cantidadDespachados++;
-        sumaDespachados += t.gananciaNeta;
+        cantidadDespachados++;//O(1)
+        sumaDespachados += t.gananciaNeta;//O(1)
         
     }
 
     public int getMayorSuperavit(){
-        return superavits.proximo().value.getID();
+        return superavits.proximo().value.getID();//O(1)
     }
 
-    public ArrayList<Integer> getCiudadesConMayorGanancias(){
-        return mayoresGanancia;
+    public ArrayList<Integer> getCiudadesConMayorGanancias(){//O(1)
+        return mayoresGanancia;//O(1)
     }
 
-    public ArrayList<Integer> getCiudadesConMayorPerdida(){
-        return mayoresPerdida;
+    public ArrayList<Integer> getCiudadesConMayorPerdida(){//O(1)
+        return mayoresPerdida;//O(1)
     }
-    public Integer getPromedio(){
-        return sumaDespachados / cantidadDespachados;
+    public Integer getPromedio(){//O(1)
+        return sumaDespachados / cantidadDespachados;//O(1)
     }
 }
